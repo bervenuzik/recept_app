b4dacaf60627fab8d38fb4b2992a97d355f83f07
Vladyslav Berveno <vladundancer@gmail.com>
2024-10-02 15:08:02 +0200

    changed homepage

--- recept_app/src/App.jsx
+++ recept_app/src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
-import GreetingPage from "./components/GreetingPage/GreetingPage";
+// import GreetingPage from "./components/GreetingPage/GreetingPage";
 import HomePage from "./components/HomePage/HomePage";
-//import CategoryPage from "./components/CategoryPage/CategoryPage";
+import CategoryPage from "./components/CategoryPage/CategoryPage";
 import {AppContext}  from "./components/AppContext/AppContext";
 
 function App() {
@@ -14,8 +14,8 @@ function App() {
         <Header />
         <main>
           <Routes>
-            <Route path="/" element={<GreetingPage />} />
-            <Route path="/home" element={<HomePage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/categories" element={<CategoryPage/>}/>
           </Routes>
         </main>
         <Footer/>
