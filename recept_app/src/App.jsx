d626e49229f5b80533148752a86042794e5ea9e7
Albin KL <proguy1998@outlook.com>
2024-10-10 13:30:34 +0200

    Routes

--- recept_app/src/App.jsx
+++ recept_app/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 // import GreetingPage from "./components/GreetingPage/GreetingPage";
 import HomePage from "./components/HomePage/HomePage";
+import TopRatedPage from "./components/TopRatedPage/TopRatedPage";
 import CategoryPage from "./components/CategoryPage/CategoryPage";
 import {AppContext}  from "./components/AppContext/AppContext";
 import RecipePage from "./components/RecipePage/RecipePage";
@@ -16,6 +17,7 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/rated" element={<TopRatedPage/>}/>
             <Route path="/categories" element={<CategoryPage/>}/>
             <Route path="/recipe/:id" element={<RecipePage/>}/>
           </Routes>
