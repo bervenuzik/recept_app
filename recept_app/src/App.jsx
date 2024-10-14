5abdaf0bdb8f3ab06d79493ee9e90bcf31943454
Vladyslav Berveno <vladundancer@gmail.com>
2024-10-14 16:50:59 +0200

    Changed Layot in App.jsx for integretion of Header in Router context. Fixed a feature for hightlightning of chosen category

--- recept_app/src/App.jsx
+++ recept_app/src/App.jsx
@@ -1,28 +1,38 @@
 //import "../normalize.css"
 import "./App.css";
 import Header from "./components/Header/Header";
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 // import GreetingPage from "./components/GreetingPage/GreetingPage";
 import HomePage from "./components/HomePage/HomePage";
 import TopRatedPage from "./components/TopRatedPage/TopRatedPage";
 import CategoryPage from "./components/CategoryPage/CategoryPage";
-import {AppContext}  from "./components/AppContext/AppContext";
+import { AppContext } from "./components/AppContext/AppContext";
 import RecipePage from "./components/RecipePage/RecipePage";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
 
 function App() {
   return (
     <AppContext>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/rated" element={<TopRatedPage/>}/>
-            <Route path="/categories/:categoryName" element={<CategoryPage/>}/>
-            <Route path="/recipe/:id" element={<RecipePage/>}/>
-          </Routes>
-        </main>
-        <Footer/>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />}/>
+            <Route path="/rated" element={<TopRatedPage />} />
+            <Route path="/categories/:categoryName" element={<CategoryPage />} />
+            <Route path="/recipe/:id" element={<RecipePage />} />
+          </Route>
+        </Routes>
     </AppContext>
   );
 }
--- recept_app/src/components/MenuItems/MenuItems.jsx
+++ recept_app/src/components/MenuItems/MenuItems.jsx
@@ -1,41 +1,54 @@
 import styles from "./MenuItems.module.css";
-import { useState, useEffect } from "react"
-import fetchData from "../../functions/fetchData"
-import { Divider, IconButton, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { useState, useEffect} from "react";
+import fetchData from "../../functions/fetchData";
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { Link, useParams } from "react-router-dom";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
 
 function MenuItems({ heading, url }) {
-    const [menuItems, setMenuItems] = useState([]);
+  const [menuItems, setMenuItems] = useState([]);
+//   const {chosenCategory , setChosenCategory}  = useContext(Context)
+  useEffect(() => {
+    fetchData(url, setMenuItems);
+  }, [url]);
 
-    useEffect(() => {
-        fetchData(url, setMenuItems);
-    }, [url]);
+const {categoryName} = useParams()
 
-    return (
-        <>
-            <List className={styles.menuItem}>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemText primary={<Typography variant="h5">{heading}</Typography>} />
-                        <ArrowForwardIcon />
-                    </ListItemButton>
+  
+  
 
-                </ListItem>
-
-                <Divider />
-
-                {menuItems.map((item, index) =>
-                    <ListItem key={item.name || index}>
-                        <ListItemButton component={Link} to={`/categories/${item.name}`}>
-                            <ListItemText primary={item.name} />
-                            <ArrowForwardIcon />
-                        </ListItemButton>
-                    </ListItem>
-                )}
-            </List>
-        </>
-    )
+  return (
+    <>
+      <List className={styles.menuItem}>
+        <ListItem>
+            <ListItemText
+              primary={<Typography variant="h5">{heading}</Typography>}
+            />
+        </ListItem>
+        <Divider />
+        {menuItems.map((item, index) => {
+        const isSelected = categoryName == item.name;
+        console.log(item.name+" " + isSelected)
+          return (
+            <ListItem className={isSelected ? styles.highlighted : ''}  key={item.name || index}>
+              <ListItemButton  component={Link} to={`/categories/${item.name}`}>
+                <ListItemText primary={item.name} />
+                <ArrowForwardIcon />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
+    </>
+  );
 }
 
-export default MenuItems
+export default MenuItems;
