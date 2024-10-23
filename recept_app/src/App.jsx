3015b96da123a1d162d35fe062e6605a3927fb7b
Vladyslav Berveno <vladundancer@gmail.com>
2024-10-23 19:49:31 +0200

    Merge branch 'master' of https://github.com/bervenuzik/recept_app

--- recept_app/src/App.jsx
+++ recept_app/src/App.jsx
@@ -1,7 +1,8 @@
 //import "../normalize.css"
 import "./App.css";
 import Header from "./components/Header/Header";
-import { Outlet, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react"; 
+import { Outlet,Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 // import GreetingPage from "./components/GreetingPage/GreetingPage";
 import HomePage from "./components/HomePage/HomePage";
@@ -9,11 +10,13 @@ import TopRatedPage from "./components/TopRatedPage/TopRatedPage";
 import CategoryPage from "./components/CategoryPage/CategoryPage";
 import { AppContext } from "./components/AppContext/AppContext";
 import RecipePage from "./components/RecipePage/RecipePage";
+import SearchResultsPage from "./components/SearchResults/SearchResults";
+import fetchAllRecipes from './functions/fetchAllRecipes'; 
 
-function Layout() {
+function Layout({recipes}) {
   return (
     <>
-      <Header />
+      <Header recipes={recipes} />
       <main>
         <Outlet />
       </main>
@@ -22,18 +25,28 @@ function Layout() {
   );
 }
 
+
 function App() {
+
+  const [recipesArray, setRecipesArray] = useState([]);
+
+  useEffect(() => {
+    fetchAllRecipes(setRecipesArray);
+  }, []);
+
   return (
     <AppContext>
         <Routes>
-          <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Layout recipes={recipesArray} />}>
             <Route index element={<HomePage />}/>
             <Route path="/rated" element={<TopRatedPage />} />
             <Route path="/categories/:categoryName" element={<CategoryPage />} />
             <Route path="/recipe/:id" element={<RecipePage />} />
+            <Route path="/search-results" element={<SearchResultsPage />} />
           </Route>
         </Routes>
     </AppContext>
   );
 }
+
 export default App;
